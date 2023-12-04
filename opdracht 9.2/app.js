6d1e1b633c532e340eff6e743f73f9d56064caee
Delvinvini <144020857+Delvinvini@users.noreply.github.com>
2023-12-04 15:01:19 +0100

    Delete opdracht 9.2 directory

--- opdracht 9.2/app.js
+++
@@ -1,7 +0,0 @@
-function toggleEmoji() {
-    const closedEmoji = document.querySelector('.emoji.closed');
-    const openEmoji = document.querySelector('.emoji.open');
-
-    closedEmoji.classList.toggle('active');
-    openEmoji.classList.toggle('active');
-}
